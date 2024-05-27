801aa07338905b4e0b3728376a1b9bff475cd7b5
yashbajaj2011 <74587202+yashbajaj2011@users.noreply.github.com>
2024-05-27 21:10:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    alert('Yay! I\'m so excited!');
+    // Optionally, you could redirect to another page
+    // window.location.href = 'somepage.html';
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    alert('Oh no! That\'s a bummer.');
+    // Optionally, you could redirect to another page
+    // window.location.href = 'anotherpage.html';
+});
